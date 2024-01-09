0217179d047ccc6e6f6235ef589f332dba5e8407
MScott-Dev <MScott0199@gmail.com>
2024-01-09 00:03:49 -0500

    Fixed get route for a specific id which wasnt working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,10 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
  try {
-  const categoryData = await Category.findAll();
+  const categoryData = await Category.findAll( { 
+    include: [{model: Product}]
+  });
+    
   res.status(200).json(categoryData);
  } catch (err) {
   res.status(500).json(err);
@@ -13,65 +16,59 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  try { const categoryData = await Category.findByPK(req.params.id, {
-    include: [{model: Category}]
+  const categoryData = await Category.findByPk(req.params.id, {
+    include: [{ model: Product}]
   });
 
-  if (!categoryData) {
-    res.status(404).json({mesage: 'No category found with that id!'});
-  }
-
-  res.status(200).json(categoryData);
-  }catch (err) {
-    res.status(500).json(err);
-  }
+  return res.json(categoryData);
 });
+ 
+  
+// router.post('/', async (req, res) => {
+//   try {
+//     const categoryData = await categoryData.create(req.body);
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
-router.post('/', async (req, res) => {
-  try {
-    const categoryData = await categoryData.create(req.body);
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.put('/:id', async (req, res) => {
+//   try {
+//     const categoryData = await categoryData.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
 
-router.put('/:id', async (req, res) => {
-  try {
-    const categoryData = await categoryData.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
+//     if (!categoryData[0]) {
+//       res.status(404).json({ message: 'No category with this id!'});
+//       return;
+//     }
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
 
-    if (!categoryData[0]) {
-      res.status(404).json({ message: 'No category with this id!'});
-      return;
-    }
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+// });
 
-});
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const categoryData = await categoryData.destory({
+//       where: {
+//         id: req.params.id
+//       }
+//     });
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const categoryData = await categoryData.destory({
-      where: {
-        id: req.params.id
-      }
-    });
+//     if (!categoryData) {
+//       res.status(404).json({ message: 'No category with that id!'});
+//       return;
+//     }
 
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category with that id!'});
-      return;
-    }
-
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.status(200).json(categoryData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
